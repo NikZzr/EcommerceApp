7abbc24b0c827fa91ff9575d0edd265138a53e6f
B12 <nnino651@unab.edu.co>
2025-04-09 09:22:08 -0500

    Login

--- app/src/main/java/com/nicolasnino/ecommerceapp/LoginScreen.kt
+++ app/src/main/java/com/nicolasnino/ecommerceapp/LoginScreen.kt
@@ -1,8 +1,10 @@
 package com.nicolasnino.ecommerceapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -10,11 +12,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -27,7 +33,10 @@ import androidx.compose.ui.unit.sp
 fun LoginScreen(){
     Scaffold { innerPadding ->
         Column (
-            modifier = Modifier.padding(innerPadding).padding(horizontal = 30.dp)
+            modifier = Modifier.padding(innerPadding).padding(horizontal = 30.dp).fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
         ){
             Image(
                 painter = painterResource(R.drawable.logo),
@@ -61,12 +70,40 @@ fun LoginScreen(){
                         },
                 shape = RoundedCornerShape(12.dp)
             )
+            Spacer(modifier = Modifier.height(28.dp))
 
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Email",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                label = {
+                    Text(text = "Correo Electronico")
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(onClick = {}, modifier = Modifier.fillMaxWidth().height(50.dp), shape = RoundedCornerShape(12.dp), colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF9900))) {
+                Text("Iniciar Sesion")
+            }
+            Spacer(modifier = Modifier.height(32.dp))
 
+            TextButton(onClick = {}) {
+                Text(
+                    text = "¿No tienes cuenta? Registrate",
+                    color = Color(0xFFFF9900))
+        }
 
+            }
         }
     }
-}
+
 
 
 @Preview
