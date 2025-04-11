822c4335fd1f34dbad60bb10b8b4296d5c5063a3
Juan Perilla <jperilla@unab.edu.co>
2025-04-11 10:03:17 -0500

    Avance de la clase-2

--- app/src/main/java/com/nicolasnino/ecommerceapp/HomeScreen.kt
+++ app/src/main/java/com/nicolasnino/ecommerceapp/HomeScreen.kt
@@ -1,12 +1,23 @@
 package com.nicolasnino.ecommerceapp
 
+import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,8 +29,12 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.rememberAsyncImagePainter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -63,6 +78,35 @@ fun HomeScreen(){
     ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
 
+            Text(text = "Promociones Destacadas",
+                modifier = Modifier
+                    .padding(
+                        top = 16.dp,
+                        start = 16.dp,
+                        bottom = 8.dp
+                    )
+                )
+
+            val listadoPromociones = listOf(
+                "https://img.freepik.com/vector-gratis/plantilla-banner-horizontal-degradado-ventas-buen-fin_23-2150873588.jpg",
+                "https://www.muycomputer.com/wp-content/uploads/2021/12/ofertas-de-invierno-de-Steam-2.jpg",
+                "https://media.vandal.net/ivandal/11/60/1146x600/11/11-2024/5/202411510554871_1.jpg",
+                "https://img.freepik.com/psd-premium/banner-promocion-venta-pc-juegos_252779-1142.jpg",
+                "https://img.freepik.com/psd-premium/banner-promocion-venta-viernes-negro-pc-juegos_252779-1151.jpg"
+            )
+
+            LazyRow (
+                modifier = Modifier.fillMaxWidth()
+                    .padding(start = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ){
+                item { CardPromo(listadoPromociones[0]) }
+                item { CardPromo(listadoPromociones[1]) }
+                item { CardPromo(listadoPromociones[2]) }
+                item { CardPromo(listadoPromociones[3]) }
+                item { CardPromo(listadoPromociones[4]) }
+            }
+
         }
     }
 }
@@ -72,5 +116,22 @@ fun HomeScreen(){
 @Composable
 fun HomeScreenPrevie(){
     HomeScreen()
+}
 
+
+@Composable
+fun CardPromo(urlImage: String) {
+    Card(modifier = Modifier
+        .height(180.dp)
+        .width(300.dp),
+        shape = RoundedCornerShape(100.dp)
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(urlImage),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+
+        )
+    }
 }
